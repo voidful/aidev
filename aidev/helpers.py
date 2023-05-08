9eeadcf34f616bdf42d57b1c12a7bc5950a242ed
voidful <voidful.stack@gmail.com>
2023-05-08 13:25:04 +0800

    feat(main): update default max-tokens value

--- aidev/helpers.py
+++ aidev/helpers.py
@@ -1,13 +1,28 @@
-# helpers.py
 import queue
 import sys
 import threading
 import time
+from contextlib import contextmanager
+from enum import Enum
 from itertools import cycle
 from typing import Optional
 
 import git
 import openai
+
+
+class ResponseType(Enum):
+    SIMPLE = "simple"
+    EXECUTION_TEST = "execution_test"
+    CODE_IMPROVEMENT = "code_improvement"
+    UNIT_TESTS = "unit_tests"
+
+
+def assert_git_repo() -> None:
+    try:
+        git.Repo('.', search_parent_directories=True)
+    except git.InvalidGitRepositoryError:
+        raise Exception("The current directory must be a Git repository!")
 
 
 def get_git_diff() -> Optional[str]:
@@ -22,11 +37,17 @@ def get_git_diff() -> Optional[str]:
         return diff_cached
 
 
-animation_running = True
-
-
-def _update_progress_bar() -> None:
+@contextmanager
+def animation_status(status: bool):
     global animation_running
+    animation_running = status
+    try:
+        yield
+    finally:
+        animation_running = not status
+
+
+def update_spinner_animation():
     spinner = cycle(['-', '\\', '|', '/'])
     print("\U0001F680 Running... ", end="", flush=True)
     while animation_running:
@@ -37,69 +58,110 @@ def _update_progress_bar() -> None:
     sys.stdout.write(" \n")
 
 
-def _call_openai_api(prompt: str, engine: str, threshold: float, language: str, max_tokens: int,
-                     response_queue: queue.Queue) -> None:
-    global animation_running
-    response = openai.ChatCompletion.create(
-        model=engine,
-        messages=[
-            {"role": "system",
-             "content": "You are a helpful code assistant."
-                        f"You need to generate Results using the following language {language}"},
-            {"role": "user",
-             "content": f"You need to generate Results using the following language {language}: {prompt}"},
-        ],
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=threshold,
-    )
+def call_openai_api(prompt: str, engine: str, threshold: float, language: str, max_tokens: int,
+                    response_queue: queue.Queue) -> None:
+    with animation_status(True):
+        response = openai.ChatCompletion.create(
+            model=engine,
+            messages=[
+                {"role": "system",
+                 "content": f"You are a helpful code assistant.You need to generate Results using the following language: {language}"},
+                {"role": "user",
+                 "content": f"{prompt}"}
+            ],
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+            temperature=threshold,
+        )
     response_queue.put(response)
-    animation_running = False
 
 
-def get_mock_run_result(code: str, threshold: float, engine: str, language: str = "english",
-                        max_tokens: int = 300) -> str:
-    if engine == "gpt-3.5-turbo":
-        MAX_INPUT_LENGTH = 3796  # preserve 300 char for response
-    elif engine == "gpt-4":
-        MAX_INPUT_LENGTH = 7892  # preserve 300 char for response
-    elif engine == "gpt-4-32k":
-        MAX_INPUT_LENGTH = 32468  # preserve 300 char for response
-    else:
-        MAX_INPUT_LENGTH = 3000
-
-    if len(code) > MAX_INPUT_LENGTH:
+def input_length_validation(code: str, max_input_length: int) -> bool:
+    if len(code) > max_input_length:
         print(f"Warning: Code change is too extensive ({len(code)} characters) for AI to handle. \n"
-              f"Please consider breaking your changes into smaller commits to less than {MAX_INPUT_LENGTH} characters.")
-        return ""
-    prompt = (
-        f"Given the following code changes:\n\n{code}\n\n"
-        f"Answer the following questions with its title and keep it short, divide each answer with lines:\n"
-        f"- Code Execution Test: Can the modified code run correctly? Provide a mock run and test result for the following code changes:\n"
-        f"- Code Improvement: Are the changes an improvement? If not, what suggestions do you have to improve the code?\n"
-        f"- Commit Message: Provide a commit message following the AngularJS Git Commit Guidelines with type, scope (optional) and subject. Use imperative, present tense, and do not capitalize the first letter of the subject or end it with a period.\n"
-        f"- Unit Tests: Provide example code of unit tests to cover the changes made to the code, but no need write test on documentation change:\n"
-        f"Results:\n"
-    )
+              f"Please consider breaking your changes into smaller commits to less than {max_input_length} characters.")
+        return False
+    return True
 
+
+def get_ai_response(code: str, response_type: ResponseType, threshold: float, engine: str, language: str = "english",
+                    max_tokens: int = 300) -> str:
+    max_input_lengths = {
+        "gpt-3.5-turbo": 3796,
+        "gpt-4": 7892,
+        "gpt-4-32k": 32468,
+    }
+    max_input_length = max_input_lengths.get(engine, 3000)
+
+    if not input_length_validation(code, max_input_length):
+        return ""
+
+    prompt_template = build_prompt_template(response_type)
+    prompt = prompt_template.format(code=code)
     response_queue = queue.Queue()
-    api_call = threading.Thread(target=_call_openai_api,
+    api_call = threading.Thread(target=call_openai_api,
                                 args=(prompt, engine, threshold, language, max_tokens, response_queue))
     api_call.start()
 
-    progress_updater = threading.Thread(target=_update_progress_bar)
+    progress_updater = threading.Thread(target=update_spinner_animation)
     progress_updater.start()
     api_call.join()
     progress_updater.join()
 
     response = response_queue.get()
-    result = response.choices[0]['message']['content'].strip()
-    return result
+    response_text = response.choices[0]['message']['content'].strip().replace("\n\n", "\n")
+    return response_text
 
 
-def assert_git_repo() -> None:
-    try:
-        git.Repo('.', search_parent_directories=True)
-    except git.InvalidGitRepositoryError:
-        raise Exception("The current directory must be a Git repository!")
+def build_prompt_template(response_type: ResponseType) -> str:
+    if response_type == ResponseType.SIMPLE:
+        return (
+            f"Given the following code changes:\n{{code}}\n"
+            f"Answer the following questions with its title and keep it as short as possible:\n"
+            f"- Commit Message: Provide a commit message, format it inside $ and following the AngularJS Git Commit Guidelines with type, scope (optional) and subject. Use imperative, present tense, and do not capitalize the first letter of the subject or end it with a period.\n"
+            f"- Code Execution Test: if they can run correctly or not? \n"
+            f"- Code Improvement: if they have further improvement or not?\n"
+            f"- Unit Tests: if unit tests are needed or not?\n"
+            f"Results:\n"
+        )
+    elif response_type == ResponseType.EXECUTION_TEST:
+        return (
+            f"Given the following code changes:\n{{code}}\n"
+            f"- Code Execution Test: Provide a mock run and some test result for the following code changes. \n"
+            f"Results:\n"
+        )
+    elif response_type == ResponseType.CODE_IMPROVEMENT:
+        return (
+            f"Given the following code changes:\n{{code}}\n"
+            f"- Code Improvement: Are the changes an improvement? If not, what suggestions do you have to improve the code? \n"
+            f"Results:\n"
+        )
+    elif response_type == ResponseType.UNIT_TESTS:
+        return (
+            f"Given the following code changes:\n{{code}}\n"
+            f"- Unit Tests: Provide example code of unit tests to cover the changes made to the code, but no need write test on documentation change.\n"
+            f"Results:\n"
+        )
+    else:
+        raise ValueError("Invalid response_type")
+
+
+def get_ai_run_result(code: str, threshold: float, engine: str, language: str = "english",
+                      max_tokens: int = 300) -> str:
+    return get_ai_response(code, ResponseType.SIMPLE, threshold, engine, language, max_tokens)
+
+
+def get_code_execution_test_detail(code: str, threshold: float, engine: str, language: str = "english",
+                                   max_tokens: int = 300) -> str:
+    return get_ai_response(code, ResponseType.EXECUTION_TEST, threshold, engine, language, max_tokens)
+
+
+def get_code_improvement_detail(code: str, threshold: float, engine: str, language: str = "english",
+                                max_tokens: int = 300) -> str:
+    return get_ai_response(code, ResponseType.CODE_IMPROVEMENT, threshold, engine, language, max_tokens)
+
+
+def get_unit_tests_detail(code: str, threshold: float, engine: str, language: str = "english",
+                          max_tokens: int = 300) -> str:
+    return get_ai_response(code, ResponseType.UNIT_TESTS, threshold, engine, language, max_tokens)
--- aidev/main.py
+++ aidev/main.py
@@ -1,9 +1,12 @@
 import locale
+import subprocess
 
 import click
 import openai
+from bullet import Bullet
 
-from aidev.helpers import get_mock_run_result, get_git_diff
+from aidev.helpers import get_git_diff, get_ai_run_result, get_code_execution_test_detail, get_code_improvement_detail, \
+    get_unit_tests_detail
 from .config_utils import read_config, store_config
 
 config = read_config() or {}
@@ -15,7 +18,7 @@ detected_language = locale.getdefaultlocale()[0][:2]
               help='Confidence threshold for the AI generated result (0.0 to 1.0).')
 @click.option('--engine', default=config.get('engine', 'gpt-3.5-turbo'),
               help='The GPT engine to use (e.g., "gpt-3.5-turbo", "gpt-4", "gpt-4-32k").')
-@click.option('--max-tokens', default=config.get('max_tokens', 300), type=int,
+@click.option('--max-tokens', default=config.get('max_tokens', 1024), type=int,
               help='The maximum number of tokens in the AI-generated response.')
 @click.option('--language', default=config.get('language', detected_language),
               help='The output language for the AI-generated response.')
@@ -46,13 +49,44 @@ def main(threshold, engine, max_tokens, language):
     if not diff:
         print("No changes detected.")
         return
-    mock_result = get_mock_run_result(diff, threshold, engine, language, max_tokens)
-    if len(mock_result) > 0:
-        print("=========")
-        print(mock_result)
-        print("=========")
+    response_text = get_ai_run_result(diff, threshold, engine, language, max_tokens)
+    while True:
+        cli = Bullet(choices=["Create a commit",
+                              "Run Code Execution Test",
+                              "Give detailed Code Improvement",
+                              "Create a unit test for this change",
+                              "Exit"],
+                     prompt=f"{response_text}\nSelect an action: ",
+                     bullet=">")
 
+        action = cli.launch()
 
-
-if __name__ == '__main__':
-    main()
+        if action == "Create a commit":
+            commit_msg = " ".join(response_text.split("$")[1:2]).strip()
+            print(f"Commit message: {commit_msg}")
+            confirm_commit = input("Do you want to commit these changes? (y/n): ").lower() == 'y'
+            if confirm_commit:
+                try:
+                    subprocess.run(["git", "add", "-A"], check=True)
+                    subprocess.run(["git", "commit", "-m", commit_msg], check=True)
+                    print("Commit created successfully.")
+                    confirm_push = input("Do you want to push the commit? (y/n): ").lower() == 'y'
+                    if confirm_push:
+                        try:
+                            subprocess.run(["git", "push"], check=True)
+                            print("Commit pushed successfully.")
+                        except subprocess.CalledProcessError as e:
+                            print(f"Error pushing commit: {e}")
+                except subprocess.CalledProcessError as e:
+                    print(f"Error creating commit: {e}")
+        elif action == "Run Code Execution Test":
+            response_text = "Code Execution Test\n"
+            response_text += get_code_execution_test_detail(diff, threshold, engine, language, max_tokens)
+        elif action == "Give detailed Code Improvement":
+            response_text = "Code Improvement\n"
+            response_text += get_code_improvement_detail(diff, threshold, engine, language, max_tokens)
+        elif action == "Create a unit test for this change":
+            response_text = "Unit Test\n"
+            response_text += get_unit_tests_detail(diff, threshold, engine, language, max_tokens)
+        elif action == "Exit":
+            break
