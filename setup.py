23bf672a9077e8026d770877deb96faf30aa69da
voidful <voidful.stack@gmail.com>
2025-02-27 09:54:52 +0800

    fix: handle exceptions when fetching available engines

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aidev",
-    version="0.9",
+    version="0.10",
     packages=find_packages(),
     install_requires=[
         "click",
